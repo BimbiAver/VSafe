8eb7c706b6853e9cf0ae368d8e50b48b65464e22
Deshan D. Abeykoon <deshangoldcity@gmail.com>
2022-04-21 00:55:35 +0530

    Initial commit 2

--- app/src/main/java/com/orionsoft/vsafe/SplashActivity.java
+++ app/src/main/java/com/orionsoft/vsafe/SplashActivity.java
@@ -2,12 +2,16 @@ package com.orionsoft.vsafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        Hide the status bar and make the activity fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
     }
 }
