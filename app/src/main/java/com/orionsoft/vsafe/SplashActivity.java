08c29e453948e6c6b5e62cdb43bb708de51bc667
Deshan D. Abeykoon <deshangoldcity@gmail.com>
2022-04-21 14:27:42 +0530

    SplashActivity Completed.

--- app/src/main/java/com/orionsoft/vsafe/SplashActivity.java
+++ app/src/main/java/com/orionsoft/vsafe/SplashActivity.java
@@ -1,7 +1,10 @@
 package com.orionsoft.vsafe;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
@@ -9,9 +12,20 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 //        Hide the status bar and make the activity fullscreen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+//        Change the activity after 2 seconds
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, ExploreActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1500); // Post back to the main Thread after 2000 Millis (2 seconds)
     }
 }
