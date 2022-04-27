98892f2b7b100ec06eab41fbc576b668031ac4d3
Deshan D. Abeykoon <deshangoldcity@gmail.com>
2022-04-27 14:09:02 +0530

    Completed upto DatePickerDialog of activity_registration.xml

--- app/src/main/java/com/orionsoft/vsafe/RegistrationActivity.java
+++ app/src/main/java/com/orionsoft/vsafe/RegistrationActivity.java
@@ -2,15 +2,48 @@ package com.orionsoft.vsafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-public class RegistrationActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText edTxtDOB;
+
+    final Calendar myCalendar= Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener date;
+
+//        -----------------------------------------------------------------------------------------------
+
+    //    Activity wide interface - onClick() method
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.edTxtDOB:
+                showDatePickerDialog();
+                break;
+            default:
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+//        -----------------------------------------------------------------------------------------------
+
+        //        Instantiate the setOnClickListener(s) at runtime
+        edTxtDOB = findViewById(R.id.edTxtDOB);
+        edTxtDOB.setOnClickListener(this);
     }
 
 //        -----------------------------------------------------------------------------------------------
@@ -19,5 +52,26 @@ public class RegistrationActivity extends AppCompatActivity {
     public void onBackPressed() {
         finish();
         overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+    }
+
+//        -----------------------------------------------------------------------------------------------
+
+    private void showDatePickerDialog(){
+        date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                setDate();
+            }
+        };
+        new DatePickerDialog(RegistrationActivity.this,R.style.DatePickerDialog,date,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    private void setDate(){
+        String myFormat="yyyy/MM/dd";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
+        edTxtDOB.setText(dateFormat.format(myCalendar.getTime()));
     }
 }
