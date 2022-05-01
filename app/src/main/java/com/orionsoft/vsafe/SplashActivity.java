f7806eee0a293c74bf5f34d9fff011084671da2c
Deshan D. Abeykoon <deshangoldcity@gmail.com>
2022-05-01 11:16:41 +0530

    Determine whether the Android app is being used for the first time or not

--- app/src/main/java/com/orionsoft/vsafe/SplashActivity.java
+++ app/src/main/java/com/orionsoft/vsafe/SplashActivity.java
@@ -3,28 +3,50 @@ package com.orionsoft.vsafe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
+    final String appStatus = null;
+
+//        -----------------------------------------------------------------------------------------------
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        -----------------------------------------------------------------------------------------------
 
 //        Hide the status bar and make the activity fullscreen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
+//        -----------------------------------------------------------------------------------------------
+
 //        Change the activity after 2 seconds
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashActivity.this, ExploreActivity.class);
-                startActivity(i);
-                finish();
+                SharedPreferences settings = getSharedPreferences(appStatus, 0);
+
+                if (settings.getBoolean("firstTime", true)) {
+                    // The app is being launched for first time, do something
+                    Intent i = new Intent(SplashActivity.this, ExploreActivity.class);
+                    startActivity(i);
+                    finish();
+
+                    // Record the fact that the app has been started at least once
+                    settings.edit().putBoolean("firstTime", false).commit();
+                } else {
+                    Intent i = new Intent(SplashActivity.this, LoginWithMobActivity.class);
+                    startActivity(i);
+                    finish();
+                }
             }
         }, 1500); // Post back to the main Thread after 2000 Millis (2 seconds)
     }
